07ce2fb035b7bb2f8a8236f1df70e50c2f5ea653
Auxority <35202343+Auxority@users.noreply.github.com>
2024-11-16 22:39:26 +0100

    fix youtube stream creation and add JSDoc to public methods and class properties

--- index.js
+++ index.js
@@ -15,13 +15,64 @@
 (() => {
     "use strict";
 
+    class SettingsStorage {
+        /**
+         * Loads a value from the settings storage.
+         * @param {string} key
+         * @returns {unknown}
+         */
+        static load(key) {
+            return GM_getValue(key);
+        }
+
+        /**
+         * Saves a value to the settings storage.
+         * @param {string} key
+         * @param {unknown} value
+         */
+        static save(key, value) {
+            GM_setValue(key, value);
+        }
+
+        /**
+         * Deletes a value from the settings storage.
+         * @param {string} key
+         * @returns {unknown}
+         */
+        static delete(key) {
+            this.save(key, "");
+        }
+    }
+
+    /**
+     * Represents the response received from the Google OAuth API.
+     */
     class AuthToken {
+        /**
+         * The access token received from the Google OAuth API.
+         * @type {string} The access token.
+         */
         accessToken;
+
+        /**
+         * The expiration time of the access token in seconds.
+         * @type {number} The expiration time in seconds.
+         */
         expiresIn;
+
+        /**
+         * The type of token received from the Google OAuth API.
+         * @type {string} The token type.
+         */
         tokenType;
 
         static EXPECTED_TOKEN_TYPE = "Bearer";
 
+        /**
+         * @param {string} accessToken
+         * @param {number} expiresIn
+         * @param {string} tokenType
+         */
         constructor(
             accessToken,
             expiresIn,
@@ -32,21 +83,11 @@
             this.tokenType = tokenType;
         }
 
-        getAccessToken() {
-            return this.accessToken;
-        }
-
-        calculateExpirationTimestamp() {
-            const now = new Date();
-            const newSeconds = now.getSeconds() + this.expiresIn;
-            now.setSeconds(newSeconds);
-            return now.getTime();
-        }
-
-        isExpired() {
-            return Date.now() > this.calculateExpirationTimestamp();
-        }
-
+        /**
+         * Deserializes the data received from the Google OAuth API into an auth token.
+         * @param {object} data The data to deserialize.
+         * @returns {AuthToken} The deserialized auth token.
+         */
         static deserialize(data) {
             try {
                 AuthTokenValidator.validate(data);
@@ -60,9 +101,28 @@
                 data.token_type,
             );
         }
+
+        /**
+         * Calculates the expiration timestamp of the access token.
+         * @returns {number} The expiration timestamp of the access token.
+         */
+        calculateExpirationTimestamp() {
+            const now = new Date();
+            const newSeconds = now.getSeconds() + this.expiresIn;
+            now.setSeconds(newSeconds);
+            return now.getTime();
+        }
+
+        getAccessToken() {
+            return this.accessToken;
+        }
     }
 
     class AuthTokenValidator {
+        /**
+         * Validates whether the data received from the Google OAuth API is a valid auth token.
+         * @param {object} data The data to validate.
+         */
         static validate(data) {
             if (!data) {
                 throw new Error("No data provided.");
@@ -111,13 +171,22 @@
     }
 
     class AuthClient {
+        /**
+         * The OAuth client ID used to authenticate the user.
+         * @type {string}
+         */
         clientId;
+
+        /**
+         * The scope of the OAuth client ID.
+         * @type {string}
+         */
+        scope;
 
         GOOGLE_AUTH_MODE = "popup";
 
         SCOPES = [
-            "https://www.googleapis.com/auth/youtube.readonly",
-            "https://www.googleapis.com/auth/youtube.upload",
+            "https://www.googleapis.com/auth/youtube",
         ];
 
         constructor(clientId) {
@@ -125,6 +194,10 @@
             this.scope = this.getScope();
         }
 
+        /**
+         * Fetches an authentication token from the Google OAuth API.
+         * @returns {Promise<AuthToken>}
+         */
         fetchAuthToken() {
             return new Promise((resolve, reject) => {
                 const googleClient = this.getGoogleClient(resolve, reject);
@@ -162,8 +235,8 @@
         constructor() { }
 
         saveLoginResponse(loginResponse) {
-            this.setValue(this.ACCESS_TOKEN_KEY, loginResponse.getAccessToken());
-            this.setValue(this.EXPIRATION_TIMESTAMP_KEY, loginResponse.calculateExpirationTimestamp());
+            SettingsStorage.save(this.ACCESS_TOKEN_KEY, loginResponse.getAccessToken());
+            SettingsStorage.save(this.EXPIRATION_TIMESTAMP_KEY, loginResponse.calculateExpirationTimestamp());
         }
 
         isUserAuthenticated() {
@@ -171,25 +244,17 @@
         }
 
         getAccessToken() {
-            return this.getValue(this.ACCESS_TOKEN_KEY);
+            return SettingsStorage.load(this.ACCESS_TOKEN_KEY);
         }
 
         hasTokenExpired() {
-            const expiryTime = this.getValue(this.EXPIRATION_TIMESTAMP_KEY);
+            const expiryTime = SettingsStorage.load(this.EXPIRATION_TIMESTAMP_KEY);
             return !expiryTime || Date.now() > expiryTime;
         }
 
-        setValue(key, value) {
-            GM_setValue(key, value);
-        }
-
-        getValue(key) {
-            return GM_getValue(key);
-        }
-
         reset() {
-            this.setValue(this.ACCESS_TOKEN_KEY, "");
-            this.setValue(this.EXPIRATION_TIMESTAMP_KEY, "");
+            SettingsStorage.delete(this.ACCESS_TOKEN_KEY);
+            SettingsStorage.delete(this.EXPIRATION_TIMESTAMP_KEY);
         }
     }
 
@@ -198,8 +263,12 @@
 
         constructor() { }
 
+        /**
+         * Retrieves the OAuth client ID from the user.
+         * @returns {string} The OAuth client ID.
+         */
         fetchClientId() {
-            const clientId = this.getValue(this.CLIENT_ID_KEY);
+            const clientId = SettingsStorage.load(this.CLIENT_ID_KEY);
             if (!clientId) {
                 return this.showClientIdPrompt();
             }
@@ -213,41 +282,56 @@
                 throw new Error("No client ID provided.");
             }
 
-            this.setClientId(clientId);
+            SettingsStorage.save(this.CLIENT_ID_KEY, clientId);
 
             return clientId;
         }
 
-        setClientId(clientId) {
-            this.setValue(this.CLIENT_ID_KEY, clientId);
-        }
-
-        setValue(key, value) {
-            GM_setValue(key, value);
-        }
-
-        getValue(key) {
-            return GM_getValue(key);
-        }
-
         reset() {
-            this.setValue(this.CLIENT_ID_KEY, "");
+            SettingsStorage.delete(this.CLIENT_ID_KEY);
         }
     }
 
     class AuthService {
+        /**
+         * Indicates whether the authentication service has been initialized.
+         * @type {boolean}
+         */
         initialized = false;
 
+        /**
+         * The token service used to manage authentication tokens.
+         * @type {TokenService}
+         */
+        tokenService;
+
+        /**
+         * The client ID service used to manage OAuth client IDs.
+         * @type {ClientIdService}
+         */
+        clientIdService;
+
         GOOGLE_AUTH_MODE = "popup";
+
         GSI_SCRIPT_URL = "https://accounts.google.com/gsi/client";
+
         REDIRECT_URI = "https://services.planningcenteronline.com";
+
         SCOPE_SEPARATOR = " ";
 
+        /**
+         * @param {TokenService} tokenService
+         * @param {ClientIdService} clientIdService
+         */
         constructor(tokenService, clientIdService) {
             this.tokenService = tokenService;
             this.clientIdService = clientIdService;
         }
 
+        /**
+         * Initializes the authentication service by injecting the Google Sign-In script.
+         * @returns {Promise<void>}
+         */
         async init() {
             if (this.initialized) {
                 return;
@@ -258,6 +342,19 @@
             await this.injectGSIScript();
         }
 
+        /**
+         * Logs the user in using Google OAuth.
+         * @returns {Promise<void>}
+         */
+        async login() {
+            if (this.tokenService.isUserAuthenticated()) {
+                console.info("User is already authenticated.");
+                return;
+            }
+
+            await this.authenticate();
+        }
+
         async injectGSIScript() {
             const script = document.createElement("script");
             script.src = this.GSI_SCRIPT_URL;
@@ -266,15 +363,6 @@
             await new Promise((resolve) => {
                 script.onload = resolve;
             });
-        }
-
-        async login() {
-            if (this.tokenService.isUserAuthenticated()) {
-                console.info("User is already authenticated.");
-                return;
-            }
-
-            await this.authenticate();
         }
 
         async authenticate() {
@@ -289,6 +377,9 @@
 
         createTokenClient() {
             const clientId = this.clientIdService.fetchClientId();
+            if (!clientId) {
+                throw new Error("OAuth client ID is missing or invalid.");
+            }
 
             return new AuthClient(clientId);
         }
@@ -304,7 +395,11 @@
         }
     }
 
-    class YouTubeService {
+    class YouTubeApiService {
+        /**
+         * The authentication service used to authenticate the user.
+         * @type {AuthService}
+         */
         authenticationService;
 
         HTTP_UNAUTHORIZED_CODE = 401;
@@ -318,26 +413,26 @@
 
         BEARER_TOKEN_PREFIX = "Bearer";
 
-        DUMMY_ENDPOINT = "/channels?part=snippet&mine=true";
-
+        /**
+         * @param {AuthService} authenticationService - The authentication service used to authenticate the user.
+         */
         constructor(authenticationService) {
             this.authenticationService = authenticationService;
         }
 
-        async init() {
-            await this.authenticationService.init();
-        }
-
-        async dummyApiRequest() {
-            console.info("Making dummy API request.");
-            await this.executeApiRequest(this.DUMMY_ENDPOINT);
-        }
-
+        /**
+         * Executes an API request to the YouTube API.
+         * @param {string} endpoint - The API endpoint to call.
+         * @param {unknown} options - The options to pass to the fetch request.
+         * @returns {Promise<unknown>} The response data from the API.
+         */
         async executeApiRequest(endpoint, options) {
             const url = this.buildUrl(endpoint);
 
             if (!options) {
                 options = this.getRequestOptions();
+            } else if (!options.headers[this.AUTHORIZATION_HEADER_KEY]) {
+                options.headers[this.AUTHORIZATION_HEADER_KEY] = this.getBearerToken();
             }
 
             try {
@@ -348,10 +443,6 @@
             }
         }
 
-        buildUrl(endpoint) {
-            return `${this.YOUTUBE_API_BASE_URL}${endpoint}`;
-        }
-
         async handleResponse(res, endpoint, options) {
             if (res.ok) {
                 const data = await res.json();
@@ -359,12 +450,17 @@
                 return data;
             } else if (this.isUnauthorized(res.status)) {
                 await this.authenticationService.login();
-                options.headers.set(this.AUTHORIZATION_HEADER_KEY, this.getBearerToken());
+                options.headers[this.AUTHORIZATION_HEADER_KEY] = this.getBearerToken();
+                // This delay is here to prevent the API from being spammed with requests if the user is not authenticated.
                 await this.delay(this.RETRY_DELAY_MS);
                 return await this.executeApiRequest(endpoint, options);
             }
 
             throw new Error("Failed to fetch YouTube data.");
+        }
+
+        buildUrl(endpoint) {
+            return `${this.YOUTUBE_API_BASE_URL}${endpoint}`;
         }
 
         delay(ms) {
@@ -398,14 +494,148 @@
         }
     }
 
+    /**
+     * Represents a YouTube stream that can be uploaded to YouTube.
+     */
+    class YouTubeStream {
+        /**
+         * The title of the stream.
+         * @type {string}
+         */
+        title;
+
+        /**
+         * The description of the stream.
+         * @type {string}
+         */
+        description;
+
+        /**
+         * The scheduled start time of the stream.
+         * @type {Date}
+         */
+        startTime;
+
+        /**
+         * The visibility status of the stream.
+         * @type {string}
+         */
+        visibility;
+
+        PUBLIC_STREAM_VISIBILITY = "public";
+        UNLISTED_STREAM_VISIBILITY = "unlisted";
+        PRIVATE_STREAM_VISIBILITY = "private";
+
+        /**
+         * @param {string} title - The title of the stream.
+         * @param {Date} startTime - The scheduled start time of the stream.
+         */
+        constructor(title, startTime) {
+            this.title = title;
+            this.startTime = startTime;
+            this.visibility = this.PRIVATE_STREAM_VISIBILITY;
+        }
+
+        getTitle() {
+            return this.title;
+        }
+
+        setTitle(title) {
+            this.title = title;
+        }
+
+        getDescription() {
+            return this.description;
+        }
+
+        setDescription(description) {
+            this.description = description;
+        }
+
+        getStartTime() {
+            return this.startTime;
+        }
+
+        setStartTime(startTime) {
+            this.startTime = startTime;
+        }
+
+        getVisibility() {
+            return this.visibility;
+        }
+
+        setVisibility(visibility) {
+            this.visibility = visibility;
+        }
+    }
+
+    /**
+     * A service that interacts with the YouTube API to create and manage streams.
+     */
+    class YouTubeStreamService {
+        /**
+         * The YouTube API service used to interact with the YouTube API.
+         * @type {YouTubeApiService}
+         */
+        apiService;
+
+        DUMMY_ENDPOINT = "/channels?part=snippet&mine=true";
+
+        CREATE_STREAM_ENDPOINT = "/liveBroadcasts?part=snippet,status";
+
+        /**
+         * @param {YouTubeApiService} apiService
+         */
+        constructor(apiService) {
+            this.apiService = apiService;
+        }
+
+        /**
+         * @deprecated
+         * Just a dummy API request to test the API connection and authentication.
+         */
+        async dummyApiRequest() {
+            console.info("Making dummy API request.");
+            await this.apiService.executeApiRequest(this.DUMMY_ENDPOINT);
+        }
+
+        /**
+         * Uploads a stream to YouTube.
+         * @param {YouTubeStream} stream
+         */
+        async uploadStream(stream) {
+            console.info("Creating YouTube stream.");
+            await this.apiService.executeApiRequest(this.CREATE_STREAM_ENDPOINT, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    snippet: {
+                        title: stream.getTitle(),
+                        description: stream.getDescription(),
+                        scheduledStartTime: stream.getStartTime().toISOString(),
+                    },
+                    status: {
+                        privacyStatus: stream.getVisibility(),
+                    }
+                }),
+            });
+        }
+    }
+
     (async () => {
         const tokenService = new TokenService();
         const clientIdService = new ClientIdService();
-        const auth = new AuthService(tokenService, clientIdService);
-        const youtubeManager = new YouTubeService(auth);
+        const authService = new AuthService(tokenService, clientIdService);
+        const apiService = new YouTubeApiService(authService);
+        const youtubeStreamService = new YouTubeStreamService(apiService);
 
-        await youtubeManager.init();
+        await authService.init();
 
-        await youtubeManager.dummyApiRequest();
+        await youtubeStreamService.dummyApiRequest();
+
+        // const stream = new YouTubeStream("Test Stream", new Date());
+        // await youtubeStreamService.uploadStream(stream);
     })();
 })();
