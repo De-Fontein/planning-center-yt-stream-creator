615b5050f59eb07bff9f2c477cb66fdd13c8bd90
Auxority <35202343+Auxority@users.noreply.github.com>
2025-03-26 19:54:47 +0100

    use semantic versioning

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlanningCenter YouTube Integration
 // @namespace    https://github.com/Auxority/planningcenter-yt-stream-creator
-// @version      2025-03-26
+// @version      v1.0.0
 // @description  Allows you to create a YouTube stream from a PlanningCenter service plan.
 // @author       Auxority
 // @match        https://services.planningcenteronline.com/*
