2e9fd5f856c64025bfe19dc47260b7c5c3e71791
Auxority <35202343+Auxority@users.noreply.github.com>
2025-03-26 21:33:26 +0100

    Revert "use semantic versioning"
    This reverts commit 615b5050f59eb07bff9f2c477cb66fdd13c8bd90.

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlanningCenter YouTube Integration
 // @namespace    https://github.com/Auxority/planningcenter-yt-stream-creator
-// @version      v1.0.0
+// @version      2025-03-26
 // @description  Allows you to create a YouTube stream from a PlanningCenter service plan.
 // @author       Auxority
 // @match        https://services.planningcenteronline.com/*
